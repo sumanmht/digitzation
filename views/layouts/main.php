7cef4259db00a74b686dd07c8e9c3d1cf433fc51
SUMAN <elmoboy30@gmail.com>
2023-07-08 09:31:46 +0545

    .

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,6 +36,8 @@ $this->registerJsFile(Yii::$app->request->baseUrl . '/js/image.js', ['depends' =
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kalimati&display=swap" rel="stylesheet">
+
 
     <title><?= Html::encode($this->title) ?></title>
 
